eeff223923cd87dc56da902c0bd9fde4f44bfdea
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-04-19 08:41:27 +0200

    zaliczenie

--- proba.py
+++ proba.py
@@ -1,1 +1,27 @@
-print(type(2))
+l1 = [1,2,3]
+l2 = [1,2,3,4]
+
+l4=list(map(lambda a,b: a+b, l1, l2))
+l4.append(l2[-1])
+print(l4)
+#
+# l3 = [i + j for i, j in zip(l1, l2)]
+# # if len(l1)==len(l2):
+# #     l3=l3
+# # elif len(l2) > len(l1):
+# #     l3.append(l2[-1])
+# # elif len(l1) > len(l2):
+# #     l3.append(l1[-1])
+#
+# print('l3',l3)
+# # x = (1,2)
+# # print(tuple(x))
+# l5 = [None] * 4
+# print('l5',l5)
+# l4=list(map(lambda a,b: a+b, l1, l2))
+# print(type(l4))
+# l5.append(4)
+# print('l4',l4)
+# print('l5',l5)
+# print(type(l5))
+# print(type(l2[-1]))
--- zaliczenie/zad.4.py
+++ zaliczenie/zad.4.py
@@ -6,16 +6,53 @@ Klasa powinna zawierać metody magiczne __add__, __radd__, __getitem__ oraz __se
 '''
 
 class Polynomial:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, count_factors):
+        self.factors_ = [None]  * count_factors
+        ''' factors_ to jest lista wspolczynnikow wielomianu count_factors okresla
+        ile tych wspolczynnikow jest'''
+
+    def __setitem__(self, factor_number, value):
+        self.factors_[factor_number] = value
+
+    def __getitem__(self, factor_number):
+        return self.factors_[factor_number]
 
     def __repr__(self):
-        return f'{self.a}-{self.b}'
+        return f'{self.factors_}'
 
     def __add__(self, other):
-        return f' Suma wielomianów: s({self.a + other.a}, {self.b + other.b})'
+        sum_factors = [i + j for i, j in zip(self.factors_, other.factors_)]
+        if len(self.factors_) == len(other.factors_):
+            return [i + j for i, j in zip(self.factors_, other.factors_)]
+        elif len(self.factors_) > len(other.factors_):
+            sum_factors = [i + j for i, j in zip(self.factors_, other.factors_)]
+            sum_factors.append(self[-1])
+            return sum_factors
+        elif len(self.factors_) < len(other.factors_):
+                sum_factors = [i + j for i, j in zip(self.factors_, other.factors_)]
+                sum_factors.append(other[-1])
+                return sum_factors
 
-p1 = Polynomial(1,2)
-p2 = Polynomial(3,4)
-print(p1+p2)
+
+
+
+p1 = Polynomial(6)
+p2 = Polynomial(5)
+p1[0] = 2
+p1[1] = 21
+p1[2] = 23
+p1[3] = 24
+p1[4] = 24
+p1[5] = 24
+
+p2[0] = 2
+p2[1] = 21
+p2[2] = 23
+p2[3] = 24
+p2[4] = 24
+print(p1)
+print(p2)
+p3= p1+p2
+print(p3[0])
+
+
